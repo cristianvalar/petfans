2e1e2aff9e03df0e29a29a90fb78bf52fc91b9d5
Cristian <cristian@nbeta.cl>
2026-04-02 22:30:43 -0300

    fix: add EMAIL_TIMEOUT to prevent gunicorn worker kill on SMTP hang

--- petfans/settings/base.py
+++ petfans/settings/base.py
@@ -109,4 +109,5 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', '587'))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_TIMEOUT = 10
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
